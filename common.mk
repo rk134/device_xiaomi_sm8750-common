a182dce97c1ce5b2ae3904c667fda28120019c9c
rahul <rahul.kantrapally@gmail.com>
2026-06-26 18:12:19 +0100

    sm8750-common: power: Re-brand to xiaomi_sm8750

--- common.mk
+++ common.mk
@@ -331,7 +331,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
-$(call soong_config_set,power_libperfmgr,mode_extension_lib,//$(LOCAL_PATH):libpowermode-ext-onyx)
+$(call soong_config_set,power_libperfmgr,mode_extension_lib,//$(LOCAL_PATH):libpowermode-ext-xiaomi_sm8750)
 
 # Properties
 PRODUCT_COPY_FILES += \
